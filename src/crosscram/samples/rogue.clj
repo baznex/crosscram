a6566948b9086abea4175c9d7ea37e2b19188a8a
Brian Gerstle <bgerstle@spotify.com>
2014-08-24 22:47:37 -0400

    cell/move generators

--- src/crosscram/samples/rogue.clj
+++ src/crosscram/samples/rogue.clj
@@ -4,18 +4,15 @@
 
 (defn covered?
   "Returns a 1 if either cell of mm is contained in vv, otherwise 0."
-  [[mm0 mm1] vv]
-  (if (or (some #{mm0} vv)
-          (some #{mm1} vv))
-      1
-      0))
+  [mm vv]
+  (if (some (set mm) vv) 1 0))
 
 (defn coverage
    "Return the number of moves blocked by a domino."
-   [mm enemy-moves]
-   (apply + (map #(covered? mm %) enemy-moves)))
+   [mm opp-moves]
+   (apply + (map #(covered? mm %) opp-moves)))
 
  (defn make-move [game]
    (let [my-moves (game/available-moves (:board game))
-         enemy-moves (game/available-moves (game/transpose (:board game)))]
-     (first (sort-by #(coverage % enemy-moves) > my-moves))))
+         opp-moves (game/available-moves (game/transpose (:board game)))]
+     (first (sort-by #(coverage % opp-moves) > my-moves))))
